1e08354664a6319f303f3e8a054e69594cd0b4d5
José Nicodemos Maia Neto <jose+github.com@nicomaia.com.br>
2026-07-10 09:39:24 -0300

    fix: read addresses fully and treat domain length as unsigned

--- src/main/java/br/com/nicomaia/server/protocol/SocketReader.java
+++ src/main/java/br/com/nicomaia/server/protocol/SocketReader.java
@@ -2,37 +2,42 @@ package br.com.nicomaia.server.protocol;
 
 import br.com.nicomaia.server.net.Address;
 import br.com.nicomaia.server.net.AddressType;
+import java.io.EOFException;
 import java.io.IOException;
 import java.io.InputStream;
 
 public class SocketReader {
 
   public static Address readAddress(AddressType addressType, InputStream in) throws IOException {
-    byte[] buffer;
+    int length;
 
     if (AddressType.IPV6 == addressType) {
-      buffer = new byte[16];
+      length = 16;
     } else if (AddressType.IPV4 == addressType) {
-      buffer = new byte[4];
+      length = 4;
     } else if (AddressType.DOMAIN_NAME == addressType) {
-      buffer = new byte[readDomainLength(in)];
+      length = readDomainLength(in);
     } else {
       throw new IOException("Unsupported address type: " + addressType);
     }
 
-    in.read(buffer);
-    return new Address(buffer, addressType);
+    return new Address(readFully(in, length), addressType);
   }
 
   public static int readPort(InputStream in) throws IOException {
-    byte[] buffer = new byte[2];
-    in.read(buffer);
+    byte[] buffer = readFully(in, 2);
     return ((buffer[0] & 0xFF) << 8) | (buffer[1] & 0xFF);
   }
 
-  private static byte readDomainLength(InputStream in) throws IOException {
-    byte[] buffer = new byte[1];
-    in.read(buffer);
-    return buffer[0];
+  private static int readDomainLength(InputStream in) throws IOException {
+    return readFully(in, 1)[0] & 0xFF;
+  }
+
+  private static byte[] readFully(InputStream in, int length) throws IOException {
+    byte[] buffer = in.readNBytes(length);
+    if (buffer.length != length) {
+      throw new EOFException("Unexpected EOF: expected " + length + " bytes, got " + buffer.length);
+    }
+    return buffer;
   }
 }
